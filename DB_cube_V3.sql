bdbc435eb40163b059d97108d0c6b6f3f8d5a473
Juan Camilo González B <80796615+jcgonzalezb@users.noreply.github.com>
2022-03-09 21:15:12 -0500

    Update DB_cube_V3.sql

--- DB_cube_V3.sql
+++ DB_cube_V3.sql
@@ -1,11 +1,11 @@
 -- Drop database
-DROP DATABASE IF EXISTS cube_test_db;
+DROP DATABASE IF EXISTS cubeTestDb;
 
 -- Create database + user if doesn't exist
-CREATE DATABASE IF NOT EXISTS cube_test_db;
+CREATE DATABASE IF NOT EXISTS cubeTestDb;
 FLUSH PRIVILEGES;
 
-USE cube_test_db;
+USE cubeTestDb;
 
 --
 -- Table structure for table `admin`
@@ -35,13 +35,24 @@ CREATE TABLE `industry` (
   PRIMARY KEY (`industryId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+
 --
--- Table structure for table `startup_general`
+-- Dumping data for table `industry`
+--
+LOCK TABLES `industry` WRITE;
+/*!40000 ALTER TABLE `industry` DISABLE KEYS */;
+INSERT INTO `industry` VALUES ('01','Proptech'),('02','LegalTech'),('03','HealthTech'),('04','TravelTech'),('05','FinTech'),('06','MarketPlace'),('07','E-Commerce'),('08','EvenTech'),('09','SAAS');
+/*!40000 ALTER TABLE `industry` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+--
+-- Table structure for table `startupGeneral`
 --
 
-DROP TABLE IF EXISTS `startup_general`;
-CREATE TABLE `startup_general` (
-  `pymeId` varchar(60) NOT NULL,
+DROP TABLE IF EXISTS `startupGeneral`;
+CREATE TABLE `startupGeneral` (
+  `startupId` varchar(60) NOT NULL,
   `name` varchar(128) NOT NULL,
   `photoUrl` varchar(128) NOT NULL,
   `country` varchar(60) NOT NULL,
@@ -52,8 +63,19 @@ CREATE TABLE `startup_general` (
   `femaleFounders` SMALLINT NOT NULL,
   `industry` varchar(60) NOT NULL,
   `active` BOOLEAN NOT NULL DEFAULT TRUE,
-  PRIMARY KEY (`pymeId`),
+  PRIMARY KEY (`startupId`),
   FOREIGN KEY (`industry`) REFERENCES `industry` (`industryId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Table structure for table `permission`
+--
+
+DROP TABLE IF EXISTS `permission`;
+CREATE TABLE `permission` (
+  `permissionId` varchar(60) NOT NULL,
+  `permissionRight` varchar(128) NOT NULL,
+  PRIMARY KEY (`permissionId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -70,75 +92,32 @@ CREATE TABLE `user` (
   `photoUrl` varchar(128) NOT NULL,
   `phone` varchar(128) NOT NULL,
   `emailAddress` varchar(128) NOT NULL,
+  `right` varchar(60) NOT NULL,
+  `userBasicId` varchar(60) NOT NULL,
+  `accessUser` varchar(128) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  FOREIGN KEY (`userBasicId`) REFERENCES `admin` (`adminId`),
+  FOREIGN KEY (`userBasicId`) REFERENCES `startupGeneral` (`startupId`),
+  FOREIGN KEY (`right`) REFERENCES `permission` (`permissionId`),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Table structure for table `user_startup`
---
-
-DROP TABLE IF EXISTS `user_startup`;
-CREATE TABLE `user_startup` (
-  `userId` varchar(60) NOT NULL,
-  `rights` varchar(128) NOT NULL,
-  `pymeId` varchar(60) NOT NULL,
-  FOREIGN KEY (`userId`) REFERENCES `user` (`id`),
-  FOREIGN KEY (`pymeId`) REFERENCES `startup_general` (`pymeId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Table structure for table `user_cube`
+-- Table structure for table `kpiRegister`
 --
 
-DROP TABLE IF EXISTS `user_cube`;
-CREATE TABLE `user_cube` (
-  `userId` varchar(60) NOT NULL,
-  `rights` varchar(128) NOT NULL,
-  `adminId` varchar(60) NOT NULL,
-  FOREIGN KEY (`adminId`) REFERENCES `admin` (`adminId`),
-  FOREIGN KEY (`userId`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Table structure for table `authentication`
---
-
-DROP TABLE IF EXISTS `authentication`;
-CREATE TABLE `authentication` (
-  `userId` varchar(60) NOT NULL,
-  `accessUser` varchar(128) NOT NULL,
-  `password` varchar(60) NOT NULL,
-  FOREIGN KEY (`userId`) REFERENCES `user_startup` (`userId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
---
--- Table structure for table `authen_admin`
---
-
-DROP TABLE IF EXISTS `authen_admin`;
-CREATE TABLE `authen_admin` (
-  `adminId` varchar(60) NOT NULL,
-  `accessUser` varchar(128) NOT NULL,
-  `password` varchar(60) NOT NULL,
-  FOREIGN KEY (`adminId`) REFERENCES `user_cube` (`userId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Table structure for table `kpi_updating`
---
-
-DROP TABLE IF EXISTS `kpi_updating`;
-CREATE TABLE `kpi_updating` (
+DROP TABLE IF EXISTS `kpiRegister`;
+CREATE TABLE `kpiRegister` (
   `date` date NOT NULL,
-  `pymeId` varchar(60) NOT NULL,
+  `startupId` varchar(60) NOT NULL,
   `revenue` FLOAT NOT NULL,
   `ARR` FLOAT NOT NULL,
   `EBITDA` FLOAT NOT NULL,
   `GMV` FLOAT NOT NULL,
-  `number_employees` SMALLINT NOT NULL,
-  `fundraising` FLOAT NOT NULL,
+  `numberEmployees` SMALLINT NOT NULL,
+  `fundRaising` FLOAT NOT NULL,
   `CAC` FLOAT NOT NULL,
-  `active_clients` mediumint NOT NULL,
-  FOREIGN KEY (`pymeId`) REFERENCES `startup_general` (`pymeId`)
+  `activeClients` mediumint NOT NULL,
+  FOREIGN KEY (`startupId`) REFERENCES `startupGeneral` (`startupId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
