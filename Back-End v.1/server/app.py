1c7983da6e44f48262ff727908bf4de77cadf386
Juan Bocanegra <jbocane6@gmail.com>
2022-03-10 23:25:10 -0500

    [UPDATE] resolve json to return when sign-in

--- Back-End v.1/server/app.py
+++ Back-End v.1/server/app.py
@@ -10,6 +10,7 @@ from models.industry import Industry
 from models.startupGeneral import StartupGeneral
 from models.permission import Permission
 from models.user import User
+import json
 
 ma = Marshmallow(app)
 db.create_all()
@@ -59,6 +60,17 @@ def get_users():
     all_users = User.query.all()
     user_result = user_schemas.dump(all_users)
     return jsonify(user_result)
+
+@app.route('/user_pyme/<id>', methods=['GET'])
+def get_user_pyme(id):
+    id_user = User.query.get(id) # returns an User object
+    result = user_schema.dump(id_user) # return a writable json
+    pyme_result = StartupGeneral.query.get(result["userBasicId"])
+    [result.pop(key) for key in ['accessUser', 'password']]
+    result_dict = startup_general_schema.dump(pyme_result)
+    [result_dict.pop(key) for key in ['founders', 'femaleFounders', 'industry']]
+    result['pyme'] = result_dict
+    return result
 
 if __name__ == "__main__":
     app.run(debug=True)
