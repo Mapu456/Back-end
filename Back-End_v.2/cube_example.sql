b5d8fb3a01511bd1313556384c7915c1f56214e4
jcgonzalezb <jcgonzalez@gmail.com>
2022-03-20 11:42:23 -0500

    new user_testing

--- Back-End_v.2/cube_example.sql
+++ Back-End_v.2/cube_example.sql
@@ -73,7 +73,8 @@ INSERT INTO `user` VALUES
 ('TM3','Tom','Keen', 'Bogota', 'Colombia','www.google.com', '573111234500', '789@gmail.com', 'fwew323', 0),
 ('AB4','Ana','Buving', 'Bogota', 'Colombia', 'www.google.com', '573111233300', '012@gmail.com', '3423kdqdq', 0),
 ('JD5','Jack','Dole', 'Mexico DF', 'Mexico', 'www.google.com', '573111234500', '456@gmail.com', '89712kdsa', 0),
-('82bbeca4-a6fc-480a-9950-2c6454b8d596','complete','access', 'Bogota', 'Colombia','www.google.com', '573111236930', '111@gmail.com', 'sha256$tgphlrrnsDYJUWKo$c98266f95cc86a3fdd5a51fe075685a736be5a98600f88ae8efd148a075745b3', 1)
+('82bbeca4-a6fc-480a-9950-2c6454b8d596','complete','access', 'Bogota', 'Colombia','www.google.com', '573111236930', '111@gmail.com', 'sha256$tgphlrrnsDYJUWKo$c98266f95cc86a3fdd5a51fe075685a736be5a98600f88ae8efd148a075745b3', 1),
+('e76fb764-e02e-4054-aa41-fab816bf5189','user','access', 'Bogota', 'Colombia','www.google.com', '573111236930', '809@gmail.com', 'sha256$Hn5LCYhGVWgwr7p7$4d82f4508fcabb79230766bff8e509174b50c8618493b4cf5e0ed36b20ed4477', 0)
 ;
 UNLOCK TABLES;
 
